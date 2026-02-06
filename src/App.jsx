17b0c0d7386eb1ac0b21a96dcbc454d2d9ec421a
ADSO MAÑANA <juanjose35323@gmail.com>
2026-02-06 10:15:51 -0500

    RamaJ

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomeBase from "./pages/HomeBase";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -7,31 +7,39 @@ import Dashboard from "./pages/Admin/Dashboard";
 import AdminConductores from "./pages/Admin/AdminConductores";
 import AdminUsuarios from "./pages/Admin/AdminUsuarios";
 import AdminVehiculos from "./pages/Admin/AdminVehiculos";
+import { AuthProvider, useAuth } from "./pages/context/AuthContext";
 import Viajes from "./pages/Admin/ViajesAdmin";
 import "./App.css";
-import HomeCliente from "./pages/Usuarios/HomeCliente";
 import RegisterDocumentacion from "./pages/Documents";
 import AdminDocuments from "./pages/Admin/AdminDocuments";
-import InternalChat from "./components/chat/InternalChat";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeCliente />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/admin/conductores" element={<AdminConductores />} />
-        <Route path="/admin/usuarios" element={<AdminUsuarios />} />
-        <Route path="/admin/vehiculos" element={<AdminVehiculos />} />
-        <Route path="/admin/viajes" element={<Viajes />} />
-        <Route path="/documentacion" element={<RegisterDocumentacion />} />
-        <Route path="/admin/documentacion" element={<AdminDocuments />} />
-        <Route path="/chat" element={<InternalChat />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeBase />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<RequiredAuth><Dashboard /></RequiredAuth>} />
+          <Route path="/admin/conductores" element={ <AdminConductores />} />
+          <Route path="/admin/usuarios" element={<RequiredAuth> <AdminUsuarios /></RequiredAuth>} />
+          <Route path="/admin/vehiculos" element={<RequiredAuth> <AdminVehiculos /></RequiredAuth>} />
+          <Route path="/admin/viajes" element={<RequiredAuth> <Viajes/></RequiredAuth>} />
+          <Route path="/documentacion" element={ <RegisterDocumentacion />}/>
+          <Route path="/admin/documentacion" element={<RequiredAuth> <AdminDocuments/></RequiredAuth>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
 export default App;
+
+function RequiredAuth({ children }) {
+  const { token } = useAuth();
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+}
